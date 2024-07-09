4de4390cfd46e580ec75a81cd45623e7e2bd0c70
imakhlaq <imakhlaqXD@gmail.com>
2024-07-09 19:19:28 +0530

    adding db

--- api/apiv1/handlers/user_handler.go
+++ api/apiv1/handlers/user_handler.go
@@ -2,20 +2,25 @@ package handlers
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/imakhlaq/hotelreservation/db"
 	"github.com/imakhlaq/hotelreservation/types"
 )
 
-func HandleUser(c *fiber.Ctx) error {
+type UserHandler struct {
+	userStore db.UserStore //userStore is interface so u can embed any struct that satisfy the interface
+}
+
+func (u UserHandler) HandleUser(c *fiber.Ctx) error {
 
 	id := c.Params("id") // id from req param
 
 	return c.JSON(map[string]string{"name": "Akhlaq Ahmad"})
 }
-func HandleUsers(c *fiber.Ctx) error {
-	u := &types.User{
+func (u UserHandler) HandleUsers(c *fiber.Ctx) error {
+	user := &types.User{
 		ID:        "1021039019318",
 		FirstName: "Akhlaq",
 		LastName:  "Ahmad",
 	}
-	return c.JSON(u)
+	return c.JSON(user)
 }
--- db/user_store.go
+++ db/user_store.go
@@ -3,6 +3,7 @@ package db
 import "github.com/imakhlaq/hotelreservation/types"
 
 //whatever you are using need to provide this interface implementation
+//u can have multiple databases in production u just need to implement this methods
 type UserStore interface {
 	GetUserByID(string) (*types.User, error)
 }
